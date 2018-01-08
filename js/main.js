8c1d1afccf22fd44fb4080fed1f23292c508c5ec
Joseph Pacheco <josephpacheco@Josephs-MacBook-Pro.local>
2018-01-08 15:25:04 -0800

    creadted game logic

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
-cards.length;
 var cardsInPlay = [];
-cardsInPlay.length;
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-
-console.log("User flipped" + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-
-console.log("User flipped" + cardTwo);
-
-if (cardsInPlay.length === 2) {
-if (cardsInPlay[0] === cardsInPlay[1]) {
-   	alert("you found a match");
-   } else {
-   	alert("sorry try again");
-    }
+var checkForMatch = function () {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("you found match");
+  } else {
+    alert("sorry try again");
+  }
+};
+// created flipCard function
+var flipCard = function (cardId) {
+  console.log("user flipped " + cards[cardId]); 
+cardsInPlay.push(cards[cardId]);
+   if (cardsInPlay[0] === cardsInPlay[2]) {
+  checkForMatch();
 }
+}
+
+flipCard(0);
+flipCard(2);
